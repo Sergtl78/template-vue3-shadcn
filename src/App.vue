2f129f08eedcda9df708c08557f0c59ea35a7f00
Sergtl78 <sergtl78@gmail.com>
2024-10-15 11:56:02 +0300

    add shadcn

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import ModeToggle from './components/ModeToggle.vue'
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <nav class="flex gap-4 h-14 items-center">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <ModeToggle />
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <main class="container bg-red-500">
+    This is Home page
+    <Button>Click</Button>
+  </main>
+</template>
